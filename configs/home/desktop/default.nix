44328020af8a5868cd89161f4412b989d87b862f
Prince Junior Mguni <prince.mguni@outlook.com>
2024-08-18 10:34:03 +0200

    home file structure fix

--- configs/home/desktop/default.nix
+++ configs/home/desktop/default.nix
@@ -6,37 +6,6 @@
     ../../shared.nix
   ];
 
-
-  ###Services####
-  services.jellyfin = {
-    enable = true;
-    openFirewall = true;
-    user = "prince";
-  };
-
-  ###Settings####
-  programs = {
-    git = {
-      enable = true;
-      userName = "Prince Junior Mguni";
-      userEmail = "prince.mguni@outlook.com";
-      extraConfig = {
-        init = {
-          defaultBranch = "main";
-        };
-        safe = {
-          directory = "/etc/nixos";
-        };
-      };
-    };
-    steam = {
-      enable = true;
-      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    };
-  };
-
-  ###Packages####
   home-manager.users.prince =
     { pkgs, unstable, inputs, system, ... }:
     let
@@ -52,6 +21,36 @@
         ] ++ upkgs;
       };
 
-    };
+      ###Services####
+      services.jellyfin = {
+        enable = true;
+        openFirewall = true;
+        user = "prince";
+      };
+      ##############
 
+      ###Settings####
+      programs = {
+        git = {
+          enable = true;
+          userName = "Prince Junior Mguni";
+          userEmail = "prince.mguni@outlook.com";
+          extraConfig = {
+            init = {
+              defaultBranch = "main";
+            };
+            safe = {
+              directory = "/etc/nixos";
+            };
+          };
+        };
+        steam = {
+          enable = true;
+          remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+          dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+        };
+      };
+     ################
+    };
+  system.stateVersion = "23.11";
 }
