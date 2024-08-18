0d5b6627883449ad7035698ae15dbf1d65a98d1a
Prince Junior Mguni <prince.mguni@outlook.com>
2024-08-18 10:24:19 +0200

    removed gtk

--- configs/shared-desktop.nix
+++ configs/shared-desktop.nix
@@ -44,21 +44,6 @@
   hardware.xpadneo.enable = true;
   ##Bluetooth
 
-  gtk = {
-    iconTheme = {
-      name = "elementary-Xfce-dark";
-      package = pkgs.elementary-xfce-icon-theme;
-    };
-    theme = {
-      name = "adwaita-dark";
-      package = pkgs.adw-gtk3;
-    };
-       gtk4.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
-    };
-  };
   programs.dconf.enable = true;
   programs = {
     kdeconnect = {
