525699db868c4ad8f7418c52c6fafc51bd89f61e
Prince Junior Mguni <princem@bestforu.co.za>
2024-08-18 22:02:18 +0200

    removed steam from laptop

--- configs/home/laptop/default.nix
+++ configs/home/laptop/default.nix
@@ -42,13 +42,7 @@
       ################
     };
   ###Settings####
-  programs = {
-    steam = {
-      enable = true;
-      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    };
-  };
+  programs = { };
   ###Settings####
 
   ###Services####
